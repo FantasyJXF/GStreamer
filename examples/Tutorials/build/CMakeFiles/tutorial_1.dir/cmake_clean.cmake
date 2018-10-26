a004f6de2d8e7c6a6d1e4ebb0a86b25b379fa988
fantasy <jingxiaofei1@jd.com>
2018-10-26 11:12:13 +0800

    create the repo

---
+++ examples/Tutorials/build/CMakeFiles/tutorial_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutorial_1.dir/basic_tutorial-1.c.o"
+  "tutorial_1.pdb"
+  "tutorial_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tutorial_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
