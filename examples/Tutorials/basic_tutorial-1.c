707ab9fdc1b8de6569254f9c115a5c39e41267d7
fantasy <jingxiaofei1@jd.com>
2018-10-27 11:02:48 +0800

    update .gitignore

--- examples/Tutorials/basic_tutorial-1.c
+++ examples/Tutorials/basic_tutorial-1.c
@@ -1,4 +1,6 @@
 #include <gst/gst.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char * argv[])
 {
@@ -8,7 +10,9 @@ int main(int argc, char * argv[])
 
     gst_init(&argc, &argv);
 
-    pipeline = gst_parse_launch("playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm",  NULL);
+    //pipeline = gst_parse_launch("playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm",  NULL);
+    // 在file:// 之后添加文件的路径
+    pipeline = gst_parse_launch("playbin uri=file:///home/fantasy/Downloads/hongkong.mp4", NULL);
 
     gst_element_set_state(pipeline, GST_STATE_PLAYING);
 
@@ -17,6 +21,7 @@ int main(int argc, char * argv[])
     // Wait until error or EOS
     msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
 
+    printf("the msg.timestamp is %lu", msg->timestamp);
     if(msg != NULL)
     {
         gst_message_unref(msg);
